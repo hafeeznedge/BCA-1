39387d64290f3ec8feb8533f67313c095a40ffc7
hafeeznedge <hafeeznedge@gmail.com>
2025-06-19 22:47:33 +0530

    C++ Program-2

---
+++ Program2.cpp
@@ -0,0 +1,10 @@
+// Welcome to C++ Programming 
+#include <iostream>
+ int main () 
+ 
+    {
+        std::cout<<"Name : Mohammed Hafeez \n";
+        std::cout<<"Age : 17 Years \n";
+        std::cout<<"Program : BCA ";
+        return 0;
+    }
