d7d253f1a6ad31b71dd3de49254d8ed7bab738f1
hafeeznedge <hafeeznedge@gmail.com>
2025-06-20 23:26:44 +0530

    C++ Program-3

---
+++ Program3.cpp
@@ -0,0 +1,14 @@
+// Program to add 2 numbers
+#include<iostream>
+int main ()
+
+	{
+		int num1=10;
+		int num2=5;
+		int sum;
+		
+		sum= num1+num2;
+		
+		std::cout<<"The sum of 2 numbers is =" << sum;
+		
+	}
