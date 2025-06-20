87b0d43ae541b5edc3f614e22747f428944783f3
hafeeznedge <hafeeznedge@gmail.com>
2025-06-20 23:17:27 +0530

    C Language Program-3

---
+++ Program3.c
@@ -0,0 +1,17 @@
+//Program to add 2 numbers
+#include <stdio.h>
+
+int main() {
+    // Declare and initialize two integer variables
+    int num1 = 15; // First number
+    int num2 = 25; // Second number
+    int sum;       // Variable to store the sum
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Print the result
+    printf("The sum of %d and %d is %d\n", num1, num2, sum); //%d is a format specifier
+
+    return 0;
+}
