71103fdd24fe54ab803cf96292834493c0cb65d7
hafeeznedge <hafeeznedge@gmail.com>
2025-06-30 21:48:08 +0530

    Program 9 c++ using for loop

---
+++ program9.cpp
@@ -0,0 +1,10 @@
+//program for printing 1-5 using for loop
+#include <iostream>
+
+int main() {
+    std::cout << "Numbers from 1 to 5:" << std::endl;
+    for (int i = 1; i <= 5; ++i) { //for loop conditions
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
