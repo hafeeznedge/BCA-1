80a794aed3bdcff023602fca55a7ddf058aac7f4
hafeeznedge <hafeeznedge@gmail.com>
2025-07-25 22:00:39 +0530

    Program 30 Destructors

---
+++ Program31.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Test {
+public:
+    // User-Defined Constructor
+    Test() {
+        cout << "Constructor Called"
+             << endl;
+    }
+    // User-Defined Destructor
+    ~Test() {
+        cout << "Destructor Called"
+             << endl;
+    }
+};
+main() {
+    Test t;
+    return 0;
+}
