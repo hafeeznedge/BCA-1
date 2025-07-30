ca73598d6b39601382aee4a36ae321f57a2eb231
hafeeznedge <hafeeznedge@gmail.com>
2025-07-30 21:49:07 +0530

    Program 35

---
+++ Program35.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ class NotOp {
+   private:
+    int a;
+    bool b;
+   public:
+    NotOp() : a(0), b(true) {} // inititalising values for the private data members
+     void in() {
+        cout << "Enter the first number : ";
+        cin >> a;
+        cout<< "Enter true or false : ";
+        cin >> b;    }
+     // Overloading the NOT (!)  operator
+    void operator ! () {
+        a= !a;
+        b= !b;    }
+    void out() {
+      cout<<"Output: "<<endl<< a<<endl<<b;    }};
+ int main() {
+    NotOp obj;
+   obj.in();
+    !obj;
+    obj.out();
+    //true or false will be given as 1 and 0 
+     return 0
