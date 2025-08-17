1660707576fb226f0ca6df49b2e3560a4ccf3120
hafeeznedge <hafeeznedge@gmail.com>
2025-08-17 22:32:04 +0530

    prgm 49 learn

---
+++ Program 49.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class Math {
+    int num;
+public:
+    // Set value
+    void setValue(int val) { num = val; }
+    // Overload + operator
+    Math operator + (Math& obj) {
+        Math temp;
+        temp.num = num + obj.num;
+        return temp;    }
+    // Overload - operator
+    Math operator - (Math& obj) {
+        Math temp;
+        temp.num = num - obj.num;
+        return temp;    }
+    // Overload * operator
+    Math operator * (Math& obj) {
+        Math temp;
+        temp.num = num * obj.num;
+        return temp;    }
+    // Overload / operator
+    Math operator / (Math& obj) {
+        Math temp;
+        temp.num = num / obj.num;
+        return temp;    }
+    // Display value
+    void display() const {
+        cout << num << endl;    }};
+int main() {
+    Math obj1, obj2, result;
+    obj1.setValue(20);
+    obj2.setValue(10);
+    cout << "Obj1: ";
+    obj1.display();
+    cout << "Obj2: ";
+    obj2.display();
+    // Perform and display operations
+    result = obj1 + obj2;
+    cout << "Obj1 + Obj2: ";
+    result.display();
+    result = obj1 - obj2;
+    cout << "Obj1 - Obj2: ";
+    result.display();
+    result = obj1 * obj2;
+    cout << "Obj1 * Obj2: ";
+    result.display();
+    result = obj1 / obj2;
+    cout << "Obj1 / Obj2: ";
+    result.display();
+    return 0;}
