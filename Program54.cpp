15aa1816cf89f449e61957b3d1fe4f2d76f05cf9
hafeeznedge <hafeeznedge@gmail.com>
2025-08-23 19:06:18 +0530

    Program 54

---
+++ Program54.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Property {
+private:
+    string name;
+    string location;
+    int price;
+
+public:
+    // Constructor
+    Property(string n, string l, int p) {
+        name = n;
+        location = l;
+        price = p;
+    }
+
+    // Operator overloading (+)
+    Property operator+(Property &p) {
+        // Adding prices of two properties
+        Property temp("Combined Property", "Multiple Locations", this->price + p.price);
+        return temp;
+    }
+
+    // Display function
+    void display() {
+        cout << "Property Name: " << name << endl;
+        cout << "Location: " << location << endl;
+        cout << "Price: " << price << endl;
+        cout << "---------------------------" << endl;
+    }
+};
+
+int main() {
+    Property p1("Sunshine Apartments", "Bangalore", 5000000);
+    Property p2("Green Villas", "Hyderabad", 7500000);
+
+    cout << "Property 1 Details:" << endl;
+    p1.display();
+
+    cout << "Property 2 Details:" << endl;
+    p2.display();
+
+    // Using overloaded + operator
+    Property total = p1 + p2;
+
+    cout << "After Adding Both Properties:" << endl;
+    total.display();
+
+    return 0;
+}
